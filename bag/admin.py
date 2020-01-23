a4165299e9a180500c64bf47274b444eb5e89b33
vique254 <victoronyango051@gmail.com>
2020-01-23 10:35:35 +0300

    register sites

--- bag/admin.py
+++ bag/admin.py
@@ -2,5 +2,7 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-
+from .models import category,Image
 # Register your models here.
+admin.site.register(category)
+admin.site.register(Image)
